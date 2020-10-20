4c2c4b28f7be8a6b10db21bd5a6b717ba54d0f7c
Charlie Theodore <charliestableford2@gmail.com>
2020-10-20 12:25:18 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const body = document.querySelector('body');
+
+const background = () => `
+<div class="background">
+<div class="background1"><img src="imgs/ontop.png"></div>
+<div class="text">What plants are best for attracting bees?</div>
+
+<div class="cloud1"><img src="imgs/cloud1.png"></div>
+<div class="cloud2"><img src="imgs/cloud2.png"></div>
+<div class="cloud3"><img src="imgs/cloud3.png"></div>
+<div class="cloud4"><img src="imgs/cloud4.png"></div>
+<div class="cloud5"><img src="imgs/cloud5.png"></div>
+<div class="cloud6"><img src="imgs/cloud6.png"></div>
+<div class="cloud7"><img src="imgs/cloud7.png"></div>
+<div class="cloud8"><img src="imgs/cloud8.png"></div>
+<div class="cloud9"><img src="imgs/cloud9.png"></div>
+<div class="cloud10"><img src="imgs/cloud10.png"></div>
+<div class="cloud11"><img src="imgs/cloud11.png"></div>
+<div class="cloud12"><img src="imgs/cloud12.png"></div>
+<div class="cloud13"><img src="imgs/cloud13.png"></div>
+<div class="cloud14"><img src="imgs/cloud14.png"></div>
+<div class="cloud15"><img src="imgs/cloud15.png"></div>
+<div class="bubble1"><img src="imgs/bubble1.png"></div>
+<div class="bubble2"><img src="imgs/bubble2.png"></div>
+<div class="bubble3"><img src="imgs/bubble3.png"></div>
+<div class="bubble4"><img src="imgs/bubble4.png"></div>
+<div class="bubble5"><img src="imgs/bubble5.png"></div>
+<div class="bubble6"><img src="imgs/bubble6.png"></div>
+<div class="bee"><img src="imgs/bee1.png"></div>
+<div class="bee1"><img src="imgs/bee1.png"></div>
+<div class="bee2"><img src="imgs/bee2.png"></div>
+<div class="bee3"><img src="imgs/bee3.png"></div>
+<div class="bee4"><img src="imgs/bee4.png"></div>
+<div class="bee5"><img src="imgs/bee5.png"></div>
+<div class="bee6"><img src="imgs/bee6.png"></div>
+<div class="bee7"><img src="imgs/bee7.png"></div>
+<div class="bee8"><img src="imgs/bee8.png"></div>
+<div class="bee9"><img src="imgs/bee5.png"></div>
+<div class="bee10"><img src="imgs/bee5.png"></div>
+<div class="bee11"><img src="imgs/bee5.png"></div>
+<div class="bee12"><img src="imgs/bee5.png"></div>
+<div class="bee13"><img src="imgs/bee5.png"></div>
+<div class="bee14"><img src="imgs/bee5.png"></div>
+<div class="bee15"><img src="imgs/bee5.png"></div>
+<div class="bee16"><img src="imgs/bee5.png"></div>
+<div class="bee17"><img src="imgs/bee5.png"></div>
+<div class="bee18"><img src="imgs/bee5.png"></div>
+<div class="bee19"><img src="imgs/bee5.png"></div>
+<div class="bee20"><img src="imgs/bee5.png"></div>
+<div class="bee21"><img src="imgs/bee5.png"></div>
+<div class="textBtm">So... why are they so important? As pollinators, bees play a part in every aspect of the ecosystem. They support the growth of trees, flowers, and other plants, which serve as food and shelter for creatures large and small. Bees contribute to complex, interconnected ecosystems that allow a diverse number of different species to co-exist.<br>
+    There is no doubting the importance of bees to our food supply. Without them, our gardens would be bare and our plates empty.</div>
+</div>
+`;
+
+body.innerHTML = background;
